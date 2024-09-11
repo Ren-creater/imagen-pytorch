c544fb7ce677f706307d3ca663e56bbec1026463
Zhibo Ren <zhiboren630@gmail.com>
2024-09-11 14:43:07 +0100

    retain_grad

--- imagen_pytorch/imagen_pytorch.py
+++ imagen_pytorch/imagen_pytorch.py
@@ -2180,8 +2180,10 @@ class Imagen(nn.Module):
             xa = x_start[:, :, :cond_video_frames.shape[2]]
             xb = x_start[:, :, cond_video_frames.shape[2]:]
             losses = F.mse_loss(xa, cond_video_frames, reduction = 'none')
+            #losses.requires_grad_()
             def gradient_wrt_zb(losses):
                 # Clear previous gradients if any
+                x.retain_grad()
                 if x.grad is not None:
                     x.grad.zero_()
                 # Compute the gradient of the loss with respect to x
